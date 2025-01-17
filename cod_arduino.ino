2576ec8ff15b18f76278f27e28ac040ea02b96f0
DrStrange <94751177+GeoFlorinVasilache@users.noreply.github.com>
2025-01-17 03:38:42 +0200

    Update cod_arduino.ino

--- cod_arduino.ino
+++ cod_arduino.ino
@@ -1,16 +1,31 @@
 #define AO_PIN A0  // Pin analogic pentru AO
+#include <DHT.h>
+
+#define DHTPIN 2          // Pinul la care este conectat senzorul DATA
+#define DHTTYPE DHT11     // Tipul senzorului
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);  // Inițializează comunicația serială
   pinMode(AO_PIN, INPUT);
+  dht.begin();
 }
 
 void loop() {
   int analogValue = analogRead(AO_PIN);  // Citește valoarea analogică
-
+  float temperature = dht.readTemperature();
+  // Verifică dacă citirea este validă
+  if (isnan(temperature)) {
+    Serial.println("Eroare la citirea senzorului!");
+    return;
+  }
   // Trimite datele către Raspberry Pi
-  Serial.print("Umiditate citita: ");
-  Serial.println(analogValue);  // Trimite valoarea pe o linie separată
+  Serial.print("Umiditate: ");
+  Serial.print(analogValue);  // Trimite valoarea pe o linie separată
+  Serial.print(" ");
+  Serial.print("Temperatura: ");
+  Serial.println(temperature);
 
   Serial.flush();  // Asigură-te că datele sunt trimise complet
 
